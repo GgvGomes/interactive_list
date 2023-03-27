88b0ad53605d8b4bf0f736fdd4c2af5c467b6803
GgvGomes <ggvgabriel05@gmail.com>
2023-03-27 17:27:57 -0300

    testes

--- src/components/movableCard.tsx
+++ src/components/movableCard.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 import Animated, {
   SharedValue,
@@ -6,6 +6,7 @@ import Animated, {
   useSharedValue,
   useAnimatedStyle,
   withSpring,
+  useAnimatedGestureHandler,
 } from "react-native-reanimated";
 import { movableCard } from "../styles/components/movableCard";
 import { Card, Card_Props } from "./cards";
@@ -13,9 +14,12 @@ import { Card, Card_Props } from "./cards";
 import {
   Gesture,
   GestureDetector,
+  LongPressGestureHandler,
   PanGestureHandler,
 } from "react-native-gesture-handler";
 import { Cards } from "../styles/_abstract";
+import { Platform } from "react-native";
+import { useLongPress } from "use-long-press";
 
 export function MovabelCard({
   title,
@@ -28,46 +32,46 @@ export function MovabelCard({
   scrollY: SharedValue<number>;
   cardsCount: number;
 }) {
+  //   const opacity = useSharedValue(0.4);
   const [moving, setMoving] = useState(false);
   const top = useSharedValue(cardsPosition.value[id] * Cards.CARDS_HEIGHT);
 
   const longPressGesture = Gesture.LongPress()
     .onStart((event) => {
       runOnJS(setMoving)(true);
+
+      //   opacity.value = withSpring(opacity.value + 0.6);
       console.log("Long press started");
     })
+    // .onFinalize(() => runOnJS(setMoving)(true))
     .minDuration(500);
 
-  useEffect(() => {
-    console.log(moving, "fui alterado");
-  }, [moving]);
-
   const dragGesture = Gesture.Pan()
-  .manualActivation(true)
-  .onTouchesDown((event, state) => {
+    .manualActivation(true)
+    .onTouchesDown((event, state) => {
       moving == true ? state.activate() : state.fail();
-  })
-  .onUpdate((event) => {
+    })
+    .onUpdate((event) => {
       // console.log(event)
       top.value = event.absoluteY + scrollY.value;
-  })
-  .onFinalize(() => {
-    runOnJS(setMoving)(false);
-  });
+    })
+    .onFinalize(() => {
+      runOnJS(setMoving)(false);
+    });
 
   const animatedStyle = useAnimatedStyle(() => {
     return {
       top: top.value - Cards.CARDS_HEIGHT,
-      opacity: withSpring(moving ? 1 : 0.4),
+      //   opacity: opacity,
+    //   opacity: withSpring(moving ? 1 : 0.4),
+      opacity: moving ? 1 : 0.4,
       zIndex: moving ? 1 : 0,
     };
   }, [moving]);
 
-  const composed = Gesture.Race(longPressGesture, dragGesture);
-
   return (
     <Animated.View style={[movableCard.container, animatedStyle]}>
-      <GestureDetector gesture={composed}>
+      <GestureDetector gesture={longPressGesture}>
         <Card title={title} id={id} />
       </GestureDetector>
     </Animated.View>
