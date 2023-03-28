74cb1eac319f64720efeb2e37254d94a994b2387
GgvGomes <ggvgabriel05@gmail.com>
2023-03-28 08:47:35 -0300

    App concluido

--- src/components/movableCard.tsx
+++ src/components/movableCard.tsx
@@ -7,6 +7,7 @@ import Animated, {
   useAnimatedStyle,
   withSpring,
   useAnimatedGestureHandler,
+  useAnimatedReaction,
 } from "react-native-reanimated";
 import { movableCard } from "../styles/components/movableCard";
 import { Card, Card_Props } from "./cards";
@@ -18,6 +19,7 @@ import {
   PanGestureHandler,
 } from "react-native-gesture-handler";
 import { Cards } from "../styles/_abstract";
+import { Cards_Object } from "../static/objetcs";
 
 export function MovabelCard({
   title,
@@ -34,6 +36,35 @@ export function MovabelCard({
   const [moving, setMoving] = useState(false);
   const top = useSharedValue(cardsPosition.value[id] * Cards.CARDS_HEIGHT);
 
+  function objectMove(positions: number[], from: number, to: number) {
+    "worklet";
+    const newPostion = Object.assign({}, positions);
+    console.log(newPostion);
+
+    for (const id in positions) {
+      if (positions[id] == from) {
+        newPostion[id] = to;
+      }
+
+      if (positions[id] == to) {
+        newPostion[id] = from;
+      }
+    }
+
+    return newPostion;
+  }
+
+  useAnimatedReaction(
+    () => cardsPosition.value[id], // observa quando acontece animação nele
+    (currentPosition, previousPosition) => {
+      if (currentPosition !== previousPosition) {
+        if (!moving)
+          top.value = withSpring(currentPosition * Cards.CARDS_HEIGHT);
+      }
+    },
+    [moving]
+  );
+
   const longPressGesture = Gesture.LongPress()
     .onStart((event) => {
       runOnJS(setMoving)(true);
@@ -45,21 +76,48 @@ export function MovabelCard({
 
   const dragGesture = Gesture.Pan()
     .manualActivation(true)
-    .onTouchesDown((event, state) => {
+    .onTouchesMove((event, state) => {
       moving == true ? state.activate() : state.fail();
     })
     .onUpdate((event) => {
-      top.value = event.absoluteY + scrollY.value;
+      const positionY = event.absoluteY + scrollY.value;
+      top.value = positionY - Cards.CARDS_HEIGHT - 20;
+      // - 20 é para ficar mais centralizado
+
+      const startPositionList = 0;
+      const endPositionList = cardsCount - 1; // descartando 1 do length
+      const currentPosition = Math.floor(positionY / Cards.CARDS_HEIGHT);
+
+      const newPosition = Math.max(
+        startPositionList,
+        Math.min(currentPosition, endPositionList)
+      );
+
+      // Eu acho q isso já funcionava
+      // const newPosition = currentPosition;
+
+      'worklet';
+      if (newPosition != cardsPosition[id]) {
+        cardsPosition.value = objectMove(
+          cardsPosition.value,
+          cardsPosition.value[id],
+          newPosition
+        );
+      }
     })
     .onFinalize(() => {
+      const newPosition = cardsPosition.value[id] * Cards.CARDS_HEIGHT;
+      top.value = withSpring(newPosition);
+
       runOnJS(setMoving)(false);
-    });
+    })
+    .simultaneousWithExternalGesture(longPressGesture);
 
   const animatedStyle = useAnimatedStyle(() => {
     return {
       top: top.value - Cards.CARDS_HEIGHT,
       //   opacity: opacity,
-        // opacity: withSpring(moving ? 1 : 0.4),
+      // opacity: withSpring(moving ? 1 : 0.4),
       opacity: moving ? 1 : 0.4,
       zIndex: moving ? 1 : 0,
     };
